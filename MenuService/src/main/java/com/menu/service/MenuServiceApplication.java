a00241ec68b4dcedc6de5567fe11a3bd62281b61
Kim Boryeong <kbr3551@gmail.com>
2024-12-13 14:13:29 +0900

    feat : MenuService (API) 생성

---
+++ MenuService/src/main/java/com/menu/service/MenuServiceApplication.java
@@ -0,0 +1,17 @@
+package com.menu.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class MenuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuServiceApplication.class, args);
+	}
+
+}
